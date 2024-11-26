52637bfd945794a431cdd0ceb66130fc0fd31077
erico <ericokempfer@gmail.com>
2024-11-26 17:15:41 -0300

    login_completo

--- tela_login/src/pages/index.jsx
+++ tela_login/src/pages/index.jsx
@@ -1,63 +1,130 @@
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Button, Flex, Heading, HStack, Image, Input, Link, Stack, Box,  } from "@chakra-ui/react";
-import React from "react";
+import { Button, Flex, Heading, HStack, Image, Input, Link, Stack, Box, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox"
+import { PasswordInput, PasswordStrengthMeter, } from "@/components/ui/password-input"
+import { FaGoogle } from "react-icons/fa6";
+import { Separator } from "@chakra-ui/react"
+import { SiApple } from "react-icons/si";
+import { Icon } from "@chakra-ui/react"
+
 
 export default function Home() {
+  const [visible, setVisible] = useState(false)
   return (
     <HStack
-    w="100%"
-    h="100vh"
-    bgColor={"black"}
+      w="100%"
+      h="100vh"
+      bgColor={"gray.800"}
     >
       <Flex
-      w="200%"
-      h="full"
-      >
-        <Box 
-        bgRepeat={"no-repeat"}
-        bgPos={"center"}
-        bgImage="url(https://images6.alphacoders.com/491/491674.jpg)" 
-        w="full"
+        w="220%"
         h="full"
-        bgSize="cover"
+      >
+        <Box
+          bgRepeat={"no-repeat"}
+          bgPos={"left"}
+          bgImage="url(https://images6.alphacoders.com/491/491674.jpg)"
+          p={5}
+          w="full"
+          h="full"
+          bgSize="cover"
         />
       </Flex>
       <Flex
-      w="full"
-      h="full"
-      alignItems="center"
-      justifyContent="center"
+        w="full"
+        h="full"
+        alignItems="center"
+        justifyContent="center"
       >
         <Stack
-        w="full"
-        maxW="md"
-        p={6}
+          w="full"
+          maxW="md"
+          p={4}
         >
+          <Image
+            pb={5}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Pepsi_logo_%282014%29.svg/800px-Pepsi_logo_%282014%29.svg.png"
+            alt="Pepsi Logo"
+            maxW={"250px"}
+            w={"auto"}
+            h={"auto"}
+          />
           <Heading
-          fontSize={"2xl"}
-          color="#004B93"
+            fontSize={"2xl"}
+            color="white"
+            fontWeight={600}
           >
-          Acesse sua conta  
+          Acesse sua conta
           </Heading>
-          <FormControl id="usuario" color={"white"}>
-            <FormLabel>Usuário</FormLabel>
-            <Input placeholder="Seu usuário"/>
-          </FormControl>
-          <FormControl id="senha" color={"white"}>
-            <FormLabel>Senha</FormLabel>
-            <Input type="password"placeholder="Sua senha"/>
+          <Text pb={5} color="whiteAlpha.500" fontSize={14}>Digite suas credenciais</Text>
+          <FormControl pb={10} id="usuario" color={"white"}>
+            <FormLabel pb={5}>Usuário</FormLabel>
+            <Input borderColor="transparent" bgColor={"black"} placeholder="Seu usuário" _placeholder={{ color: "whiteAlpha.700" }} _focus={{ borderColor: "#004B93" }} />
+          </FormControl >
+          <FormControl pb={5} id="senha" color={"white"}>
+            <FormLabel pb={5}>Senha</FormLabel>
+            <PasswordInput
+              borderColor="transparent"
+              bgColor={"black"}
+              placeholder="Sua senha"
+              _placeholder={{ color: "whiteAlpha.700" }}
+              _focus={{ borderColor: "#004B93" }}
+              visible={visible}
+              onVisibleChange={setVisible}
+            />
+            <Text color="whiteAlpha.500" fontSize={13} pl={0.4} pb={0}>Password is {visible ? "visible" : "hidden"}</Text>
           </FormControl>
           <Stack
-          direction={"row"}
-          align="start"
-          justify={"space-between"}
-          spacing={4}
+            direction={"row"}
+            align="start"
+            justify={"space-between"}
+            spacing={4}
+            pb={15}
+            pl={0.5}
           >
             <Checkbox colorPalette="blue" size="md" color={"white"}>Lembrar-me</Checkbox>
             <Link color="#004B93" fontWeight={700} fontSize={14}>Esqueci minha senha</Link>
           </Stack>
-          <Button bgColor="#004B93">Entrar</Button>
+          <Button borderRadius={20} bgColor="#004B93">Entrar</Button>
+          <HStack pb={2} pt={2}>
+            <Separator borderColor='whiteAlpha.700' />
+            <Text flexShrink="0" color="whiteAlpha.700">OU</Text>
+            <Separator borderColor='whiteAlpha.700' />
+          </HStack>
+          <Button
+            borderRadius={20}
+            bgColor="white"
+            color="black"
+            alignItems="center"
+            justifyContent="center"
+            gap={0} // Adds space between icon and text
+            style={{ marginBottom: '10px' }}
+          >
+            <Icon fontSize="18px" style={{ marginLeft: '10px' }} >
+            <FaGoogle />
+            </Icon>
+            <Text pl={0} style={{ marginLeft: '10px' }}>Entrar com o Google</Text>
+          </Button>
+
+          <Button
+            borderRadius={20}
+            bgColor="white"
+            color="black"
+            alignItems="center"
+            justifyContent="center"
+            gap={2} // Adds space between icon and text
+            
+          >
+            <Icon fontSize="25px">
+            <SiApple />
+            </Icon>
+            <Text>Entrar com a Apple</Text>
+          </Button>
+          <HStack pt={2} alignSelf={"center"}>
+          <Text color={"whiteAlpha.500"}>Não tem uma conta?</Text>
+          <Link variant="underline" color={"blue.500"}>Cadastre-se</Link>
+          </HStack>
         </Stack>
       </Flex>
     </HStack>
