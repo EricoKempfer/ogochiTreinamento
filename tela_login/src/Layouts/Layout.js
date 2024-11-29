d0bb4d2e00f38365d60dd31ef1c282c360aaab97
erico <ericokempfer@gmail.com>
2024-11-29 18:25:02 -0300

    bonito

--- tela_login/src/Layouts/Layout.js
+++ tela_login/src/Layouts/Layout.js
@@ -1,51 +1,48 @@
 import { Box, HStack, Flex, VStack, Icon } from "@chakra-ui/react";
 import Fim from "../components/Fimpagina";
-import Topopagina from "../components/Topopagina"
+import Topopagina from "../components/Topopagina";
 import { TbBrandPepsi } from "react-icons/tb";
 import { AiOutlineProduct } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { BsQuestionCircle } from "react-icons/bs";
+import { FaGear } from "react-icons/fa6";
 
 const Layout = ({ children }) => {
   return (
     <div className="content">
-      <HStack
-        w="100%"
-        h="100vh"
-      >
+      <HStack w="100%" h="100vh">
         <Flex
           w="4%"
           h="full"
+          direction="column" // Use column direction to stack icons vertically
+          justify="space-between" // Push icons to the top and bottom
         >
-          <Box
-            bgColor= "white"
-            w="full"
-            h="full"
-          >
-          <Icon w={12} h={12} color={"#004B93"} marginLeft={3.5} marginTop={2} >
-          <TbBrandPepsi />
-          </Icon>
-          <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={10} >
-          <AiOutlineProduct />
-          </Icon>
-          <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={4} >
-          <CgProfile />
-          </Icon>
-          <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={4} >
-          <MdOutlineAttachMoney />
-          </Icon>
-          <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={4} >
-          <BsQuestionCircle />
-          </Icon>
-          
+          <Box bgColor="white" w="full" h="full">
+            <Icon w={12} h={12} color={"#004B93"} marginLeft={3.5} marginTop={2}>
+              <TbBrandPepsi />
+            </Icon>
+            <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={10}>
+              <AiOutlineProduct />
+            </Icon>
+            <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={4}>
+              <CgProfile />
+            </Icon>
+            <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginTop={4}>
+              <MdOutlineAttachMoney />
+            </Icon>
+          </Box>
+          {/* Move the Question Circle Icon to the bottom */}
+          <Box bgColor="white" >
+            <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginBottom={4}>
+              <BsQuestionCircle />
+            </Icon>
+            <Icon w={10} h={10} color={"gray"} marginLeft={4.5} marginBottom={4}>
+            <FaGear />
+            </Icon>
           </Box>
         </Flex>
-        <VStack
-          w="96%"
-          h="full"
-          spacing={4}
-        >
+        <VStack w="96%" h="full" spacing={4}>
           <Topopagina />
           {children}
           <Fim />
@@ -53,6 +50,6 @@ const Layout = ({ children }) => {
       </HStack>
     </div>
   );
-}
+};
 
 export default Layout;
--- tela_login/src/pages/admin/products.js
+++ tela_login/src/pages/admin/products.js
@@ -1,50 +1,80 @@
-import { Box, HStack, Heading, Stack, Table, Flex } from "@chakra-ui/react"
+import { Box, HStack, Heading, Stack, Table, Flex, Text } from "@chakra-ui/react"
 import {
   PaginationItems,
   PaginationNextTrigger,
   PaginationPrevTrigger,
   PaginationRoot,
 } from "../../components/ui/pagination"
-
+import { IconButton } from "@chakra-ui/react"
+import { LuSearch } from "react-icons/lu"
+import { IoIosAdd } from "react-icons/io";
+import { MdOutlineAdd } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const items = [
   { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
   { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-  { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
+  { id: 3, name: "Desk Chair", category: "Furniture", price: 139.99 },
   { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
   { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
 
 ]
 const products = () => {
   return (
-      <Stack width="full"  bgColor={"#d4d4d8"} color={"black"} pl={20} pr={20} borderRadius={"10px"} >
-        <Box
+    <Stack width="full" bgColor={"#d4d4d8"} color={"black"} pl={20} pr={20} borderRadius={"10px"} >
+      <Box
         borderRadius={"10px"}
-          backgroundColor={"white"}
-          p={10}>
-          <Heading fontSize={30} marginBottom={5} >Lista de Produtos</Heading>
-          <Table.Root  >
-            <Table.Header  >
-              <Table.Row backgroundColor="#a1a1aa"  >
-              <Table.ColumnHeader borderRadius={"10px 0px 0px 0px"} color={"white"}>Id</Table.ColumnHeader>
-                <Table.ColumnHeader color={"white"}>Tipo</Table.ColumnHeader>
-                <Table.ColumnHeader color={"white"}>Nome</Table.ColumnHeader>
-                <Table.ColumnHeader color={"white"} borderRadius={"0px 10px 0px 0px"}>Preço</Table.ColumnHeader>
-              </Table.Row>
-            </Table.Header >
-            <Table.Body  >
-              {items.map((item) => (
-                <Table.Row key={item.id} bgColor={"transparent"}  >
-                  <Table.Cell >{item.id}</Table.Cell>
-                  <Table.Cell>{item.name}</Table.Cell>
-                  <Table.Cell>{item.category}</Table.Cell>
-                  <Table.Cell>{item.price}</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-            </Table.Root>
+        backgroundColor={"white"}
+        p={10}>
+        <Box  >
+          <HStack >
+            <Box justify="space-between" w="full">
+              <HStack justify="space-between" w="full">
+                <Heading fontSize={30} marginBottom={"3"} >Lista de Produtos</Heading>
+                <IconButton marginBottom={"1"} textAlign="end" aria-label="Search database" variant="outline" >
+                  <LuSearch />
+                </IconButton>
+              </HStack>
+            </Box>
+            <IconButton borderRadius={10} p={2} marginBottom={"1"} textAlign="end" aria-label="Search database" variant="outline" border={"transparent"} bgColor={"green"} gap={0} >
+              <MdOutlineAdd color="white" />
+              <Text color={"white"} marginRight={2}>Adicionar Produto</Text>
+            </IconButton>
+          </HStack>
         </Box>
-      </Stack>
+        <Table.Root  >
+          <Table.Header  >
+            <Table.Row backgroundColor="#004B93"  >
+              <Table.ColumnHeader borderRadius={"10px 0px 0px 0px"} color={"white"}>Id</Table.ColumnHeader>
+              <Table.ColumnHeader color={"white"}>Tipo</Table.ColumnHeader>
+              <Table.ColumnHeader color={"white"}>Nome</Table.ColumnHeader>
+              <Table.ColumnHeader color={"white"}>Preço</Table.ColumnHeader>
+              <Table.ColumnHeader pl={8} color={"white"} borderRadius={"0px 10px 0px 0px"}>Ações</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header >
+          <Table.Body  >
+            {items.map((item) => (
+              <Table.Row key={item.id} bgColor={"transparent"}  >
+                <Table.Cell >{item.id}</Table.Cell>
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>{item.category}</Table.Cell>
+                <Table.Cell>{item.price}</Table.Cell>
+                <Table.Cell>
+                {<IconButton size="xs" bgColor={"#004B93"} aria-label="Search database" marginRight={3}>
+                  <MdEdit />
+                </IconButton>}
+                {<IconButton size="xs" bgColor={"red"} aria-label="Search database">
+                  <FaRegTrashAlt />
+                </IconButton>}
+                </Table.Cell>
+              </Table.Row>
+
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Box>
+    </Stack>
   );
 }
 
